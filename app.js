9fb9026fff6d14353b8da1baaea13ced1a507f21
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-05-28 09:48:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+// var 
+// const 
+// let 
+
+var Greetme = "Good Morning Mr Emmanuel"
+Greetme = "Good morning Mr President"
+console.log(Greetme);
+
+const PI = 22
+// PI = 500
+console.log(PI);
+
+let Music_artist = 'Davido' 
+Music_artist = 'Chioma Jesus'
+console.log(Music_artist);
+
+// Variable scope 
+{
+    var Plant = "Orange tree"
+}
+console.log(Plant);
+
+// Variable casing 
+// pascalCase 
+// CamelCase
+// SnakeCase
+
+First_Name = "snake case"
+Camelcase = "camel case"
+PASCALCASE = "pascal case"
+
+// Datatypes in javascript 
+// strings 
+// int 
+// Boolean
+// Array
+// Object
+// null
+// undefined
+
+let Tutor = 'Emmanuel'
+let tutor_age = 200
+let tutor_isactive = true || false 
+let tutor_siblings = ['Ogechi','Christain', "Miss Evenlyn", 'Emmanuel']
+let tutor_isanObject = {Name:'Emmanuel', Age:200, IsActive:true, Nationality:'Nigerian'}
+let Null = null
+console.log(tutor_siblings[0]);
+
+console.log(tutor_isanObject.Nationality);
+console.log('This is for Null ',Null);
+
+let x = undefined;
+console.log(x);
+
+let empty = ''
+console.log(empty);
+
+
+console.log(typeof(Tutor));
+console.log(typeof(tutor_age));
+console.log(typeof(tutor_isactive));
+console.log(typeof(tutor_siblings));
+
+
+
+
+
